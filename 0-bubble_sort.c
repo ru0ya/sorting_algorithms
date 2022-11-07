2cb7a47472cda65b92f6a3e79819b405a57b00f2
ru0ya <viktaangi02@gmail.com>
2022-11-07 23:23:36 +0300

    bubble-sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,8 +3,8 @@
 
 /**
  * bubble_sort - sorts an array of integers in ascending order
- *@size - size of list
- *@array - list to sort
+ *@size: size of list
+ *@array: list to sort
  *
  * Returns: null
  */
@@ -14,13 +14,15 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for(j = 0; j < (size - 1); j++)
+		for (j = 0; j < (size - 1); j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				int temp = array[j];
+				int temp
+
+				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
