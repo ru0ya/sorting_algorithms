3e5dec2cf382e4d9a890a1182829900cb363808e
ru0ya <viktaangi02@gmail.com>
2022-11-08 09:43:34 +0300

    select

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,11 +13,11 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_pos;
 
-	for(i = 0; i < size - 1; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min_pos = i;
 		{
-			for(j = i + 1; j < size; j++)
+			for (j = i + 1; j < size; j++)
 			{
 				if (array[j] < array[min_pos])
 					min_pos = j;
@@ -25,6 +25,7 @@ void selection_sort(int *array, size_t size)
 			if (min_pos != i)
 			{
 				int temp = array[i];
+
 				array[i] = array[min_pos];
 				array[min_pos] = temp;
 			}
