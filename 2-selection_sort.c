e9e5693468646b61770ff2e3112e63a4fb5e6677
ru0ya <viktaangi02@gmail.com>
2022-11-09 10:19:27 +0300

    selection-sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -26,27 +26,21 @@ void swap_ints(int *a, int *b)
 
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_pos;
+	size_t i, j;
+        int *min_pos;
 
 	if (size < 2 || array == NULL)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min_pos = i;
+		min_pos = array + i;
 		for (j = i + 1; j < size; j++)
-			{
-				if (array[j] < array[min_pos])
-					min_pos = j;
-			}
-			if (min_pos != i)
-			{
-				int temp = array[i];
-
-				array[i] = array[min_pos];
-				array[min_pos] = temp;
-			}
-		
-		print_array(array, size);
+			min_pos = (array[j] < *min_pos) ? (array + j) : min_pos;
+		if ((array + i) != min_pos)
+		{
+			swap_ints(array + i, min_pos);
+			print_array(array, size);
+		}
 	}
 }
