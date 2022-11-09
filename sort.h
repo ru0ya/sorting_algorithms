bd601bf07eff5da6c60a68ca5a454c7b34ef5a63
ru0ya <viktaangi02@gmail.com>
2022-11-09 10:04:25 +0300

    insertion-sort

--- sort.h
+++ sort.h
@@ -23,6 +23,7 @@ void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 
 /*sorting algorithm*/
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
