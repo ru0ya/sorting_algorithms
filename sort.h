3f427fc8816b437f56f34536bddbf567c1daddcd
ru0ya <viktaangi02@gmail.com>
2022-11-18 10:24:16 +0300

    oh

--- sort.h
+++ sort.h
@@ -35,5 +35,11 @@ void swap_ints(int *a, int *b);
 int lomuto_partition(int *array, size_t size, int left, int right);
 void lomuto_sort(int *array, size_t size, int left, int right);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
 
 #endif
